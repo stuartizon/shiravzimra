78f275df79e3cd44ed618979166090f89a6f012c
Stuart Izon <stuart.izon@gmail.com>
2023-03-26 06:56:12 -0400

    Add Kaddish for Festival evenings

--- data/festivalsMaariv.ts
+++ data/festivalsMaariv.ts
@@ -3,5 +3,14 @@ import { Section } from './types'
 export const festivalsMaariv: Section = {
   id: 'festivalsMaariv',
   name: "Festivals Ma'ariv",
-  pieces: []
+  pieces: [
+    {
+      id: '109a',
+      name: 'Kaddish',
+      author: 'Trad.',
+      youtubeUrl: 'https://youtu.be/qVcdtJMU4u4',
+      description: `This glorious traditional melody heralds the start of the three festivals, appearing during the evening service immediately prior to the recitation of the Festival Amida. Note that it is not intended for any of the Kaddishes during the daytime (which have their own distinctive tunes). \n
+        I would argue this is one of the best synagogue melodies of the entire year – typically the entire congregation joins in enthusiastically. Therefore it is somewhat disappointing that this Kaddish is not particularly well known outside the UK.`
+    }
+  ]
 }
