589b5228f19b6920c3eea009ca9120e66572032b
Stuart Izon <stuart.izon@gmail.com>
2023-03-26 06:48:59 -0400

    Add Ana Adonai for Sukkot

--- data/hallel.ts
+++ data/hallel.ts
@@ -11,6 +11,14 @@ export const hallel: Section = {
       youtubeUrl: 'https://youtu.be/F--utuT9dpM',
       description: `This is one of the classic Duke's Place melodies, still very prevalent across shuls in the UK, but unknown elsewhere in the world. The tune is in the major, and joyously declares "This is the day that God has made: let us rejoice and be happy upon it."\n
         Despite this being a general Hallel tune, not specifically for Pesach, I've always thought that the rising shape of the final line has some similarity to the end of Adir Hu.`
-    }
+    },
+    {
+        id: '127c',
+        name: "Ana Adonai (for Sukkot)",
+        author: 'Trad.',
+        youtubeUrl: 'https://youtu.be/qOlD70BGG80',
+        description: `Variations of this ancient traditional Sukkot theme can be found in Lewandowki, Sulzer, Baer, Mombach and many other composers’ scores, though the exact line of the melody seems to differ across communities. \n
+            Known to many as the Lulawschütteln nigun, its primary expression is as a slow Hallel melody, which is intentionally prolonged in order to shake the arba minim with sincerity. Its sombre quality contrasts with the otherwise joyous theme of the festival.  Aside from its use in Hallel, it is used by many as a seasonal leitmotif - e.g. for Mi Chamocha in Ma’ariv, Adon Olam, and Shir Hama’alot.`
+      },
   ]
 }
