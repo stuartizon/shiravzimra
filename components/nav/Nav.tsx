ab0444a6c71fd13ac3e19c998e77b3caf1b6749b
Stuart Izon <stuart.izon@gmail.com>
2023-03-28 14:15:35 +0300

    Use serif font for main heading

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,14 +1,11 @@
-import { FC } from 'react'
+import { CSSProperties, FC } from 'react'
 import { allGroups } from '../../data'
 
 const Nav: FC = () => {
   return (
     <nav className='w-screen p-5 top-0 font-semibold overflow-hidden'>
       <div className='container mx-auto'>
-        <ul
-          className='list-none flex flex-row'
-          style={{ fontFamily: 'Noto Sans' }}
-        >
+        <ul className='list-none flex flex-row'>
           <NavItem
             name="Shira v'Zimra"
             href='/#'
@@ -20,7 +17,8 @@ const Nav: FC = () => {
               name={group.name}
               key={group.id}
               href={`/#${group.sections[0]?.id || ''}`}
-              className="hidden md:inline"
+              className='hidden md:inline'
+              style={{ fontFamily: 'Noto Sans' }}
             />
           ))}
         </ul>
@@ -29,9 +27,9 @@ const Nav: FC = () => {
   )
 }
 
-const NavItem: FC<NavItemProps> = ({ name, href, className }) => (
+const NavItem: FC<NavItemProps> = ({ name, href, className, style }) => (
   <li className={`mx-2 mt-auto flex-shrink-0 ${className}`}>
-    <a href={href} className='text-white transition duration-100'>
+    <a href={href} className='text-white transition duration-100' style={style}>
       {name}
     </a>
   </li>
@@ -43,4 +41,5 @@ interface NavItemProps {
   name: string
   href: string
   className?: string
+  style?: CSSProperties
 }
