e518a5a9b435b379d8fa6b0331ff8fdff7e5ee50
Stuart Izon <stuart.izon@gmail.com>
2023-03-28 14:19:19 +0300

    Hide book title on mobile

--- components/cover/Cover.tsx
+++ components/cover/Cover.tsx
@@ -5,7 +5,7 @@ import styles from './Cover.module.css'
 const Cover: FC = () => {
   return (
     <div className={styles.cover}>
-      <h1 className='text-5xl font-bold mt-36 mb-4 '>Shira v&apos;Zimra</h1>
+      <h1 className='text-5xl font-bold mt-36 mb-4 hidden md:inline'>Shira v&apos;Zimra</h1>
       <div className='text-xl text-center mt-8'>
         A book of Jewish music for the synagogue and the home,
       </div>
@@ -13,7 +13,6 @@ const Cover: FC = () => {
         Arranged for male voice choir by
       </div>
 
-      {/* <h3 className='text-xl mt-4'>Arranged and Edited by</h3> */}
       <h3 className='text-xl mb-4'>STUART IZON</h3>
       <ul className='list-none' style={{ fontFamily: 'Noto Sans' }}>
         {allGroups.map(group => (
