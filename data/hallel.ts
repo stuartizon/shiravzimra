5bee28454c5b6a75e07f2a3a89f625d72b361878
Stuart Izon <stuart.izon@gmail.com>
2023-04-13 01:32:15 -0400

    Add Ma Ashiv for Pesach

--- data/hallel.ts
+++ data/hallel.ts
@@ -5,6 +5,14 @@ export const hallel: Section = {
   name: 'Hallel',
   pieces: [
     {
+      id: '107c',
+      name: "Ma Ashiv (for Pesach)",
+      author: 'Julius Mombach',
+      youtubeUrl: 'https://youtu.be/viIS6BoWOP8',
+      description: `Mombach's melody for Ma Ashiv opens with an iconic declamation, incorporates the Pesach leitmotif, and at the same time includes some beautiful word painting.
+        "How can I repay the Lord for all His goodness to me? I will lift the cup of salvation and call on the name of the Lord."`
+    },
+    {
       id: '111a',
       name: "Od'cha",
       author: 'Trad.',
@@ -13,12 +21,12 @@ export const hallel: Section = {
         Despite this being a general Hallel tune, not specifically for Pesach, I've always thought that the rising shape of the final line has some similarity to the end of Adir Hu.`
     },
     {
-        id: '112c',
-        name: "Ana Adonai (for Sukkot)",
-        author: 'Trad.',
-        youtubeUrl: 'https://youtu.be/qOlD70BGG80',
-        description: `Variations of this ancient traditional Sukkot theme can be found in Lewandowki, Sulzer, Baer, Mombach and many other composers’ scores, though the exact line of the melody seems to differ across communities. \n
-            Known to many as the Lulawschütteln nigun, its primary expression is as a slow Hallel melody, which is intentionally prolonged in order to shake the arba minim with sincerity. Its sombre quality contrasts with the otherwise joyous theme of the festival.  Aside from its use in Hallel, it is used by many as a seasonal leitmotif - e.g. for Mi Chamocha in Ma’ariv, Adon Olam, and Shir Hama’alot.`
-      },
+      id: '112c',
+      name: 'Ana Adonai (for Sukkot)',
+      author: 'Trad.',
+      youtubeUrl: 'https://youtu.be/qOlD70BGG80',
+      description: `Variations of this ancient traditional Sukkot theme can be found in Lewandowki, Sulzer, Baer, Mombach and many other composers’ scores, though the exact line of the melody seems to differ across communities. \n
+        Known to many as the Lulawschütteln nigun, its primary expression is as a slow Hallel melody, which is intentionally prolonged in order to shake the arba minim with sincerity. Its sombre quality contrasts with the otherwise joyous theme of the festival.  Aside from its use in Hallel, it is used by many as a seasonal leitmotif - e.g. for Mi Chamocha in Ma’ariv, Adon Olam, and Shir Hama’alot.`
+    }
   ]
 }
