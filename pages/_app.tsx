2ae260f890ca5a0fedb67f34cdc439a4f446c2b7
Stuart Izon <stuart.izon@gmail.com>
2023-04-20 00:26:06 -0400

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,20 @@ import type { AppProps } from 'next/app'
 import Nav from '../components/nav/Nav'
 import Footer from '../components/footer/Footer'
 import Content from '../components/content/Content'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Content>
-      <Nav />
-      <Component {...pageProps} />
-      <Footer />
-    </Content>
+    <>
+      <Head>
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      </Head>
+      <Content>
+        <Nav />
+        <Component {...pageProps} />
+        <Footer />
+      </Content>
+    </>
   )
 }
 
