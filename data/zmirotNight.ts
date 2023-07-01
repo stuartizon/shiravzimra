bee3d4261724d1bbfdbe1d04aea3269be3cea254
Stuart Izon <stuart.izon@gmail.com>
2023-07-01 00:25:17 -0400

    Add Yah Ribon

--- data/zmirotNight.ts
+++ data/zmirotNight.ts
@@ -6,7 +6,7 @@ export const zmirotNight: Section = {
   pieces: [
     {
       id: '512d',
-      name: "Shalom Aleichem",
+      name: 'Shalom Aleichem',
       author: 'Trad.',
       youtubeUrl: 'https://youtu.be/4irxiDEipRg',
       spotifyUrl: 'https://open.spotify.com/album/0s5LVilAl9lVivYIvCiZCT',
@@ -15,6 +15,17 @@ export const zmirotNight: Section = {
       description: `"Peace be with you, ministering angels, messengers of the Most High, sent by the King of Kings, the Holy One, Blessed be He"\n
         This Kabbalistic poem is first found in Tikunei Shabbat, a book from 1641. It is of unknown authorship, and is believed to have been written during the early 17th century. Shalom Aleichem is traditionally sung every Friday night before the Sabbath meal, and has been accepted by almost all communities. Some families recite each verse three times for emphasis.\n
         This traditional Yekkish melody with its idiosyncratic lopsided metre, is certainly older than the more well-known Israel Goldfarb melody. As per my comments from last time, the use of major tonality (as opposed to a Freygish mode) is typical of the Western Ashkenazic style.`
+    },
+    {
+      id: '521b',
+      name: 'Yah Ribon',
+      author: 'Israel Goldfarb',
+      youtubeUrl: 'https://youtu.be/ZJz6zXYY-G0',
+      spotifyUrl: 'https://open.spotify.com/album/5xfQaMDwr9VEiyvN7DwGvf',
+      appleUrl: 'https://music.apple.com/us/album/yah-ribon/1686502698',
+      amazonUrl: 'https://music.amazon.com/albums/B0C4CN1LMT',
+      description: `Yah Ribon is one of the most popular zemirot, traditionally sung on Friday night. Written in Aramaic, it was composed by the 16th-century kabbalist, Israel Najara, and is based on Daniel 3:32–33. Beginning by praising the King of Kings and describing the wonders of God's creation, the song concludes with a prayer that God redeem Israel and restore Jerusalem. \n
+        This singable, popular melody was composed by Israel Goldfarb in the mid 20th century.`
     }
   ]
 }
