2d3534e095505a1c73f0c631f30750b18e4273e5
Stuart Izon <stuart.izon@gmail.com>
2023-07-25 10:26:31 -0400

    Add Eli Tzion

--- data/tishaBav.ts
+++ data/tishaBav.ts
@@ -14,6 +14,19 @@ export const tishaBav: Section = {
       amazonUrl: 'https://music.amazon.com/albums/B0CCPRFC9B',
       description: `L'mi Evkeh is the first elegy of Tisha B'Av evening in the Sefardic tradition. The poem introduces the main themes of Tisha B'Av and enumerates the many things we weep for. The final stanza quotes from the first line of Lamentations, which is read immediately afterwards.\n
         This is a traditional Spanish/Portuguese melody. As with many other Spanish/Portuguese songs for Tisha B'Av, the tonality is in the major, which still feels somehow mournful yet hauntingly beautiful. The congregation sings the last word of every stanza, which the chazzan then repeats. The last verse is sung in a more ornamented fashion, as is standard in the Spanish/Portuguese service.`
+    },
+    {
+      id: '411',
+      name: "Eli Tzion",
+      author: 'Trad.',
+      youtubeUrl: 'https://youtu.be/vFeMSbZK-_I',
+      spotifyUrl: 'https://open.spotify.com/album/58aH3m4L2Ob3Gel1QycmAP',
+      appleUrl: 'https://music.apple.com/us/album/eli-tzion/1686509851',
+      amazonUrl: 'https://music.amazon.com/albums/B0C4CSBQZ9',
+      description: `Eli Tzion is the most well known dirge recited on Tisha B'Av. It compares Zion to a woman about to give birth – the birth is understood to relate to our eventual redemption. Eli Tzion is typically recited whilst standing, at the end of the morning Kinot.\n
+        This elegy is of unknown authorship, but the melody is at least as old as 1743 and is ubiquitous thereafter. There does not appear to be an authoritative version; there are minor variations between different composers' arrangements – several have introduced a second part to the theme, presumably to reduce the monotony of the many stanzas. This version here is based on the tune traditionally used in England, and at KAYJ.\n
+        Intriguingly, Eli Tzion is not an especially melancholic tune. Idelsohn identified the same melody in a German Catholic hymn, a Spanish folksong, and a Czechoslovakian folksong. His conclusion was that "it originated in Spain, whence it was in all likelihood carried to Central and Eastern Europe by pilgrims; and was picked up by Jewish singers likewise".\n
+        There is a custom to use the melody for L'cha Dodi on the Shabbat preceding Tisha B'Av (Chazon). However, many commentators have challenged this practice, since public mourning is generally forbidden on Shabbat. The Meiri defended it, stating that the purpose of this melody is to lead us towards repentance. Today there are varying customs: to use the melody of Eli Tzion for only the final verse of L'cha Dodi; for every verse except the last one; for all verses; or none at all. Some congregations make a concerted effort to complete Kabbalat Shabbat before sunset.`
     }
   ]
 }
