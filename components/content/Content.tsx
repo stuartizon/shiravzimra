e1fd89817a45eccd674b3765bed7032b636ef34b
Stuart Izon <stuart.izon@gmail.com>
2023-07-26 14:19:55 -0400

    Refactor main class into _app.tsx

--- components/content/Content.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react'
-import { PropsWithChildren } from 'react'
-import styles from './Contents.module.css'
-
-const Content: FC<PropsWithChildren> = ({ children }) => {
-  return <div className={styles.content}>{children}</div>
-}
-
-export default Content
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Nav from '../components/nav/Nav'
 import Footer from '../components/footer/Footer'
-import Content from '../components/content/Content'
 import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <div className='main'>
       <Head>
         <link rel='icon' type='image/x-icon' href='/favicon.ico' />
       </Head>
-      <Content>
-        <Nav />
-        <Component {...pageProps} />
-        <Footer />
-      </Content>
-    </>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
   )
 }
 
