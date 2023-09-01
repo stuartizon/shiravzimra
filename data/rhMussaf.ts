e0c606eeda0018c73c48a35b45b6e90ea7526fe3
Stuart Izon <stuart.izon@gmail.com>
2023-08-31 20:15:28 -0400

    Add Ochila

--- data/rhMussaf.ts
+++ data/rhMussaf.ts
@@ -7,6 +7,15 @@ export const rhMussaf: Section = {
   hebrewName: 'מוסף',
   pieces: [
     {
+      id: '258a',
+      name: "Ochila La'el",
+      author: 'Hillel Paley',
+      youtubeUrl: 'https://youtu.be/nOEOupnhH0A',
+      description: `Ochila La'el is a personal prayer which the chazzan reads during the Rosh Hashana and Yom Kippur Mussaf, beseeching God to support him in conveying the following prayers. It was probably composed in the period of the Amoraim and is based on several verses from Tanach (notably Proverbs 16).\n
+        The chazzan petitions "O Lord, open my lips" because "eloquent speech comes from the Lord". Whilst the text of our prayers are prescriptive, a chazzan must use music and feeling to express the deeper meaning of the words and to inspire his congregation, and for this he asks for heavenly assistance.\n
+        There is an old Nusach for this prayer, but it has become customary in many communities in our time to use this recent melody by Rabbi Hillel Paley, and thereby transform Ochila into a more communal request.`
+    },
+    {
       id: '266a',
       name: 'Halleluyah',
       author: 'David Montague Davis',
