a0b067a757bac87d01de2398732b4fbb52dc0cf2
Stuart Izon <stuart.izon@gmail.com>
2023-09-22 16:51:08 +0100

    Force inline content disposition for PDFs to fix download attribute on chrome

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const pdfWorkerPath = require.resolve(`pdfjs-dist/build/pdf.worker.min.js`)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: () => [
+    {
+      source: '/scores/:score.pdf',
+      headers: [
+        {
+          key: 'Content-Disposition',
+          value: 'inline'
+        }
+      ]
+    }
+  ],
   reactStrictMode: true,
   swcMinify: true,
   webpack: config => {
