2a44a4332495e0a916534ccb8593aa8ad26a83c8
Stuart Izon <stuart.izon@gmail.com>
2023-09-28 12:39:34 +0300

    Various small score fixes

--- data/zmirotNight.ts
+++ data/zmirotNight.ts
@@ -16,7 +16,7 @@ export const zmirotNight: Section = {
       amazonUrl: 'https://music.amazon.com/albums/B0C4D1VNFQ',
       description: `"Peace be with you, ministering angels, messengers of the Most High, sent by the King of Kings, the Holy One, Blessed be He"\n
         This Kabbalistic poem is first found in Tikunei Shabbat, a book from 1641. It is of unknown authorship, and is believed to have been written during the early 17th century. Shalom Aleichem is traditionally sung every Friday night before the Sabbath meal, and has been accepted by almost all communities. Some families recite each verse three times for emphasis.\n
-        This traditional Yekkish melody with its idiosyncratic lopsided metre, is certainly older than the more well-known Israel Goldfarb melody. As per my comments from last time, the use of major tonality (as opposed to a Freygish mode) is typical of the Western Ashkenazic style.`
+        This traditional Yekkish melody with its idiosyncratic lopsided metre, is certainly older than the more well-known Israel Goldfarb melody. The use of major tonality (as opposed to a Freygish mode) is typical of the Western Ashkenazic style.`
     },
     {
       id: '521b',
