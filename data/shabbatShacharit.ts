559235f3041440cf5b0724dc0c8799b9cf9e7c3f
Stuart Izon <stuart.izon@gmail.com>
2023-11-02 00:04:51 -0400

    Update Mim'kom'cha youtube link

--- data/shabbatShacharit.ts
+++ data/shabbatShacharit.ts
@@ -18,7 +18,7 @@ export const shabbatShacharit: Section = {
       id: '45d',
       name: "Mim'kom'cha",
       author: 'Ben Zion Shenker',
-      youtubeUrl: 'https://youtu.be/hhTEK0g8F0U',
+      youtubeUrl: 'https://youtu.be/tAXAXLRYmNU',
       description: `On a flight to Israel in 1968, the acclaimed composer Ben Zion Shenker was sitting next to the Knesset member Yitzchak Meir Levin, who described to him the many challenges his religious community were facing. Moved by what he had to say and given that Jerusalem had recently been liberated in the Six Day War, Shenker composed this treasured melody to the words of "V'lirushalayim ir'cha", notating it on a scrap of newspaper.\n
         In many congregations, this tune is adapted to the words of Mim'kom'cha from the Shacharit K'dusha. This is a particularly appropriate contrafactum as both texts long for the rebuilding of Jerusalem.`
     },
