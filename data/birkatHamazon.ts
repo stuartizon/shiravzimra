e02b830b6e1ec017f8b111ad1d19b6ed8306cd4a
Stuart Izon <stuart.izon@gmail.com>
2024-03-01 09:28:17 -0500

    Add Na'ar Hayiti Stulberger

--- data/birkatHamazon.ts
+++ data/birkatHamazon.ts
@@ -16,6 +16,13 @@ export const birkatHamazon: Section = {
       amazonUrl: 'https://music.amazon.com/albums/B0C487FFPS',
       description: `"May the Merciful One send us Elijah the prophet, who is remembered for good, and who will announce to us good tidings, salvation, and consolation."\n
         This section of Birkat Hamazon is almost universally sung to this popular tune by Haim Kirsch, the Israeli composer, conductor and music educator. Like many orally transmitted melodies, it has become distorted over time, and is rarely sung with the correct pronunciation in the original way Kirsch wrote it.`
+    },
+    {
+      id: '511a',
+      name: "Na'ar Hayiti",
+      author: 'Peshy Stulberger',
+      youtubeUrl: 'https://youtu.be/gaTj_1ijK08',
+      description: `Set to words from the end of Birkat Hamazon, this melody is much beloved in the acapella world. Originally recorded by Abie Rotenberg, but composed by Rebbetzin Peshy Stulberger.`
     }
   ]
 }
