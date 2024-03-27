8291a9a06c4d020cbf07ea7b24a9f2545124418d
Stuart Izon <stuart.izon@gmail.com>
2024-03-27 16:20:17 -0400

    Add Ladonai Haaretz Wasserzug

--- data/festivalMussaf.ts
+++ data/festivalMussaf.ts
@@ -18,6 +18,14 @@ export const festivalMussaf: Section = {
         "O gates, lift up your heads! Lift them up, you everlasting doors, so the King of glory may come in! Who is the King of glory? The LORD of hosts, He is the King of glory!" \n
         These magnificent words have many beloved musical settings, including by Lewandowski, Naumbourg, Mombach and Wasserzug. Of course these words have also inspired many non-Jewish composers, the most well known example being Handel’s “Lift up your heads”. \n
         This glorious, regal melody in a processional style is classic Mombach. Similarly to his more familiar Havu Ladonai, it is written in ternary form, with the middle section modulating to the subdominant. This is probably the most well known setting of Ladonai Ha’aretz, excluding Naumbourg’s melody which is typically reserved for Rosh Hashana/Yom Kippur.`
+    },
+    {
+      id: '143b',
+      name: "Ladonai Ha'aretz",
+      author: 'Haim Wasserzug',
+      youtubeUrl: 'https://youtu.be/vVe6ktoyJog',
+      description: `On Festivals which fall on weekdays, we sing Ladonai Ha'aretz (Psalm 24) as we carry the Torah scrolls back to the ark, following the Torah reading.\n
+        This splendid melody is understood to have been composed by Haim Wasserzug, though it doesn't appear anywhere in his magnum opus Schirei Mikdosch (and there are some other melodies for Psalm 24 there). I am personally very familiar with this setting, as it is used in both orthodox synagogues in Birmingham: Central and Singers Hill. Most other UK congregations defer to Mombach's composition. It is written in ABCC form with a dramatic opening, a lighter section beginning "He shall receive blessing" in duple time with some rather unusual harmony, and a dancelike theme for the final verses "O gates, lift up your heads!"`
     }
   ]
 }
