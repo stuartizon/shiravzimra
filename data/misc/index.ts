1247895854305a4aa8ab05532798e1569b701846
Stuart Izon <stuart.izon@gmail.com>
2024-09-14 00:52:09 -0400

    Add Shomer Yisrael Alman

--- data/misc/index.ts
+++ data/misc/index.ts
@@ -2,11 +2,12 @@ import { Group } from '../types'
 import { chanukah } from './chanukah'
 import { chuppah } from './chuppah'
 import { purim } from './purim'
+import { selichot } from './selichot'
 import { tishaBav } from './tishaBav'
 
 export const misc: Group = {
   id: 'misc',
   name: 'Miscellaneous',
   hebrewName: 'שונות',
-  sections: [chanukah, purim, tishaBav, chuppah]
+  sections: [chanukah, purim, tishaBav, selichot, chuppah]
 }
---
+++ data/misc/selichot.ts
@@ -0,0 +1,17 @@
+import { Section } from '../types'
+
+export const selichot: Section = {
+  id: 'selichot',
+  name: "Selichot",
+  shortName: "Selichot",
+  hebrewName: 'סליחות',
+  pieces: [
+    {
+      id: '426a',
+      name: "Shomer Yisrael",
+      author: 'Samuel Alman',
+      youtubeUrl: 'https://youtu.be/L_R_leUzIew',
+      description: `Alman's Shomer Yisrael is one of the most hauntingly beautiful compositions in the liturgy. It is unclear to me whether Alman wrote this specifically for Selichot, but it now seems intrinsically linked with these penitential prayers for the days leading up to Yom Kippur. Although the words come from the daily Tachanun, when they are said as part of Selichot, they take on an added resonance. We cry out to our creator with profound sorrow, “Guardian of Israel, guard the remnant of Israel, and let Israel not perish”.`
+    }
+  ]
+}
