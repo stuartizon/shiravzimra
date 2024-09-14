94bcd9a2f01087c0204175357ee3c3167d1ef7d2
Stuart Izon <stuart.izon@gmail.com>
2024-09-14 00:39:55 -0400

    Add Sh'ma Koleinu

--- data/yomKippur/maariv.ts
+++ data/yomKippur/maariv.ts
@@ -7,12 +7,20 @@ export const ykMaariv: Section = {
   hebrewName: 'מעריב',
   pieces: [
     {
-        id: '312a',
-        name: "El Melech Yosheiv",
-        author: 'Trad.',
-        youtubeUrl: 'https://youtu.be/vps8vKqF1Pc',
-        description: `El Melech Yosheiv is one of the key prayers of Selichot, introducing the 13 divine attributes, which we repeat many times over the holiday period. The text is one of the oldest parts of the Selichot liturgy, dating back to the Babylonian and Talmudic period. It imagines God as King, sitting on a throne of mercy, pardoning His people automatically due to His 13 divine attributes of mercy. While Ashkenazim begin saying Selichot from the Saturday night before Rosh Hashana, Sefardim start at the beginning of Elul.\n
-          This beautiful melody of unknown authorship, (beginning partway into the paragraph of El Melech Yosheiv) forms one of the great emotional highlights of the holiday period, particularly with its many repetitions during Kol Nidrei and N’ila, and in many communities, it has become a leitmotif for the High Holidays.`
-      },
+      id: '312a',
+      name: 'El Melech Yosheiv',
+      author: 'Trad.',
+      youtubeUrl: 'https://youtu.be/vps8vKqF1Pc',
+      description: `El Melech Yosheiv is one of the key prayers of Selichot, introducing the 13 divine attributes, which we repeat many times over the holiday period. The text is one of the oldest parts of the Selichot liturgy, dating back to the Babylonian and Talmudic period. It imagines God as King, sitting on a throne of mercy, pardoning His people automatically due to His 13 divine attributes of mercy. While Ashkenazim begin saying Selichot from the Saturday night before Rosh Hashana, Sefardim start at the beginning of Elul.\n
+        This beautiful melody of unknown authorship, (beginning partway into the paragraph of El Melech Yosheiv) forms one of the great emotional highlights of the holiday period, particularly with its many repetitions during Kol Nidrei and N’ila, and in many communities, it has become a leitmotif for the High Holidays.`
+    },
+    {
+      id: '318a',
+      name: "Sh'ma Koleinu",
+      author: 'Trad.',
+      youtubeUrl: 'https://youtu.be/qw9tNnEYXEM',
+      description: `One of the most well-known and beloved prayers is Sh'ma Koleinu, the emotional apex of Yom Kippur and introduction to the viduy confession. Though we say the opening line of Sh'ma Koleinu every day in our weekday Amida, it is never filled with as much feeling as it is on Yom Kippur. This beautiful, traditional melody is found in the Blue Book, arranged by Francis Lyon Cohen, but is not found in any earlier sources.\n
+        "Listen to our voice, Lord our God, spare us and have compassion on us, and accept in compassion and favour our prayers."`
+    }
   ]
 }
