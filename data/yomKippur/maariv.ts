02dbb4801b58dbc2655275457641d2b29342f595
Stuart Izon <stuart.izon@gmail.com>
2024-10-08 21:06:22 +0100

    Add V'al Chataim Lewandowski

--- data/yomKippur/maariv.ts
+++ data/yomKippur/maariv.ts
@@ -21,6 +21,15 @@ export const ykMaariv: Section = {
       youtubeUrl: 'https://youtu.be/qw9tNnEYXEM',
       description: `One of the most well-known and beloved prayers is Sh'ma Koleinu, the emotional apex of Yom Kippur and introduction to the viduy confession. Though we say the opening line of Sh'ma Koleinu every day in our weekday Amida, it is never filled with as much feeling as it is on Yom Kippur. This beautiful, traditional melody is found in the Blue Book, arranged by Francis Lyon Cohen, but is not found in any earlier sources.\n
         "Listen to our voice, Lord our God, spare us and have compassion on us, and accept in compassion and favour our prayers."`
+    },
+    {
+      id: '323a',
+      name: "V'al Chataim",
+      author: 'Louis Lewandowski',
+      youtubeUrl: 'https://youtu.be/GHxpQzwdGSA',
+      description: `At the end of the longer form of the Vidui confession during each of the services on Yom Kippur (except N'ila), we confess to general sins which incur various forms of sacrifices or punishments. For all these sins we ask that God pardon us, forgive us and grant us atonement.\n
+        Although some congregations always recite this section quietly, it is customary in many communities to sing this section aloud in the confession during the chazzan's repetition. Lewandowski's melody is perhaps not as euphoric as the well-known Ashamnu melody, but it is still upbeat and joyous, despite the sombre and serious nature of these words. While it is counterintuitive that we should so proudly declare these sins aloud, I think that this fits under the same rubric Rav Soloveitchik uses to explain the singing of Ashamnu:\n
+        There is a key difference between the individual's private confession and the public confession during the chazzan's repetition. When the individual confesses, they do so from a state of depression, insecurity and despair in the wake of a sin for which they have no assurance of being acquitted. In contrast, the Jewish community confesses out of a sense of confidence and even rejoicing with the singing of the Vidui.`
     }
   ]
 }
