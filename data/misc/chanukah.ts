12972d7346e3fe8a213694b88e7af331b30f6e6c
Stuart Izon <stuart.izon@gmail.com>
2024-12-29 22:19:56 -0500

    Add Mizmor Shir Rovner

--- data/misc/chanukah.ts
+++ data/misc/chanukah.ts
@@ -18,6 +18,15 @@ export const chanukah: Section = {
         There are several melodies, but this one is by far the most popular. It is certainly of non Jewish origin: a similar tune was used by Martin Luther for the 16th century hymn "Now rejoice dear Christians". Some have suggested that both melodies were based on the earlier German folk song "So weiss ich eins, dass mich erfreut, das pluemlein auff preiter heyde". Before being used for Maoz Tzur, this melody was sung to another Chanukah poem, Sh'nei Zeitim which is said by some on Shabbat Chanukah. \n
         In the 19th century, Julius Mombach modified the second line of the melody, so that it modulates to the dominant, adding impetus to the melody through tension and resolution. This variant was included in the Blue Book as traditional without Mombach's name attached, and spread to many UK congregations. In the UK this is typically how the melody is still sung today; in other places the second line is a direct repeat of the first line. Other variants do exist e.g. the Frankfurt melody has a different final line to each verse. This arrangement follows Mombach's altered melody. \n
         Like Adir Hu and Akdamut, the Maoz Tzur tune is used as a seasonal leitmotif in many communities for Mi Chamocha in Ma'ariv, Hodu and Ana in Hallel, Shir Hama'alot, Adon Olam and Yechadshehu for Rosh Chodesh (Kislev and Tevet).`
+    },
+    {
+      id: '403b',
+      name: "Mizmor Shir Chanukah Habayit",
+      author: 'Zeidel Rovner',
+      youtubeUrl: 'https://youtu.be/xRlXiFBWwMQ',
+      description: `Psalm 30 is recited daily as part of P'sukei D'zimra, but its connection to Chanukah gives it special prominence. Introduced into the daily liturgy as late as the 17th century—possibly with Chanukah in mind—this psalm resonates deeply with the festival's themes. The opening verse, "A psalm for the dedication of the Temple," aligns with the story of the rededication of the Temple on Chanukah. But its repeated focus on thanksgiving, culminating in the phrase "I will praise you" (conjugate with Todah), echoes the gratitude central to the holiday. (This is comparable to the liturgical additions of Al Hanisim in the thanksgiving sections of the Amida and Birkat Hamazon.)\n
+        While attributed to David and reflecting the emotional highs and lows of his life, Psalm 30 also draws inspiration from the Joseph narrative. Phrases like "you spared me from the pit" and "what gain would there be from his death" evoke familiar sections of Joseph's journey from despair to triumph, tying the psalm to the Torah portions of Vayeshev and Miketz, which are read during Chanukah.\n
+        Rovner's composition masterfully captures the psalm's emotional oscillation, contrasting majestic choral sections in major with introspective solo passages in minor. Through vivid word painting and dynamic contrasts, the piece reaches an electrifying climax at "You turned my sorrow into dancing," before concluding with the triumphant Odecha ("I will praise You").`
     }
   ]
 }
