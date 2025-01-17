f8c1db9610860353c6909fedaf2bf38e86a618db
Stuart Izon <stuart.izon@gmail.com>
2025-01-17 00:40:04 -0500

    Add Eshet Chayil Shenker

--- data/songs/zmirotNight.ts
+++ data/songs/zmirotNight.ts
@@ -19,6 +19,14 @@ export const zmirotNight: Section = {
         This traditional Yekkish melody with its idiosyncratic lopsided metre, is certainly older than the more well-known Israel Goldfarb melody. The use of major tonality (as opposed to a Freygish mode) is typical of the Western Ashkenazic style.`
     },
     {
+      id: '513a',
+      name: 'Eshet Chayil',
+      author: 'Ben Zion Shenker',
+      youtubeUrl: 'https://youtu.be/cRKMGuAJe1A',
+      description: `Eshet Chayil from chapter 31 of Proverbs is typically sung prior to Kiddush on Friday night. This is a relatively recent custom, introduced by the kabbalists, who interpreted the poem allegorically as a reference to the Sh'chinah (the divine presence) or to the Torah. Of course it is also recognised as praise for the lady of the house.\n
+        Ben Zion Shenker's melody for Eshet Chayil is one of the most iconic Shabbat melodies. Despite its immense popularity, it was only written in 1956, leading many to ponder what other tunes were used for Eshet Chayil prior to its introduction. The composition is set in the Ahava Rabba (Freygish) mode, with the first section emphasising the augmented interval between the flattened second and the third. The second part of the melody is really just an ornamentation over a descending scale from the sixth to the tonic. The harmonic alternation between the minor 4th and the tonic can lead to misinterpretations of the melody as being in the relative harmonic minor. I notice that many people unwittingly modify the tune for the final line of each verse to recontextualise the melody into the relative harmonic minor. However, Shenker's original composition remains in the Freygish throughout, beginning and ending on the same note.`
+    },
+    {
       id: '521b',
       name: 'Yah Ribon',
       author: 'Israel Goldfarb',
