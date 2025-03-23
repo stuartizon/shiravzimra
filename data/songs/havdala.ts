58caba07b3850109040a55f55eb4bdfceb0ce61c
Stuart Izon <stuart.izon@gmail.com>
2025-03-22 21:38:04 -0400

    Add Hamavdil Yekkish

--- data/songs/havdala.ts
+++ data/songs/havdala.ts
@@ -13,6 +13,14 @@ export const havdala: Section = {
       youtubeUrl: 'https://youtu.be/4tMquYGdAbE',
       description: `Debbie Friedman's Havdala melody has become ubiquitous across the world, across every denomination of Judaism, to the extent that many people don't realise how modern it is or who wrote it. Often it is sung at a breakneck pace, but it is instructive to listen to Friedman's original recording which has a much slower tempo, to soothe and console us as we reenter the week from the spiritual highs of Shabbat.\n
         Some also make a mistake in the melody over the word 'Haolam', which should descend not ascend (and may also play an incorrect chord here when accompanying). My observation is this error tends to be more prevalent amongst the Orthodox, who may have never listened to the original.`
+    },
+    {
+      id: '540a',
+      name: 'Hamavdil',
+      author: 'Trad.',
+      youtubeUrl: 'https://youtu.be/MDANN9n_J2c',
+      description: `Hamavdil is a piyut sung following Havdala on Saturday evening. The words were composed by Isaac Ibn Chayat, and it was originally part of the N'ila service on Yom Kippur.\n
+        This is a traditional Yekkish melody, which I learned from Daniel and Chavivah Van Der Plaat, though I have arranged the version as transcribed by Neil Levin in his Z'mirot Anthology.`
     }
   ]
 }
