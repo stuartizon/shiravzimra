257d1a22a8988c33416f0d8f62db47091828d040
Stuart Izon <stuart.izon@gmail.com>
2025-06-11 23:38:35 +0100

    Add Birkat Hamazon Nathanson

--- data/songs/birkatHamazon.ts
+++ data/songs/birkatHamazon.ts
@@ -16,6 +16,19 @@ export const birkatHamazon: Section = {
         This melody is very well known, but often sung much faster than the original composition, and in an absent-minded fashion without regard to the pathos of the words. During his visit to Israel, Rosenblatt performed this song for the 1933 movie "The Dream of My People" in a boat on the Jordan river. After hearing Rosenblatt singing it, Hayim Bialik reportedly wanted to use this tune for Hatikvah. Given its themes of redemption, Psalm 126 was also proposed by the early religious Zionists to be the national anthem of the State of Israel.`
     },
     {
+      id: '502a',
+      name: "Birkat Hamazon",
+      author: 'Moshe Nathanson',
+      youtubeUrl: 'https://youtu.be/3esqCiZHdR4',
+      description: `Nathanson's setting of Birkat Hamazon is one of the most well known Jewish melodies worldwide. As detailed by Dr Judith Kaplan Eisenstein, Nathanson originally wrote this melody for the Avot section of the Shabbat Amida, but it never caught on particularly well. When her father, Mordechai Kaplan urged Nathanson to make Birkat Hamazon more aesthetically pleasing, he repurposed this melody for the paragraph Hazan et Hakol. This may be why the melody doesn't completely fit the words of Birkat Hamazon.\n
+        Nathanson originally published the composition for the first paragraph, in 1939's Manginoth Shireinu, and extended it for the entire Grace after Meals in 1954's Rabbotai Nevareh. It was also later included in the Haggadah section of Zamru Lo (volume three) in 1974, and a version with the original Avot text appears in Zamru Lo (volume two). There are slight discrepancies between each of these published versions. Whilst the melody for the first paragraph is extremely prevalent, almost nobody uses the full setting for the entire Birkat Hamazon.\n
+        Several things are worth remarking on how this melody is commonly sung today compared with the published versions:\n
+        1. The pronunciation of "tamid lo chasar lanu v'al yechsar lanu" seems to be incorrect as the stress is on la-NU instead of LA-nu. Nathanson corrected this oversight in the Zamru Lo version, but this seems to have come too late after the melody had already caught on.\n
+        2. Another incorrect stress pattern is on "hu noten lechem l'chol basar". The word should be LE-chem, not le-CHEM. Here the reverse occurred: in the earlier publications there is a long note on "hu noten" as you will find in this arrangement, but apparently no-one wanted to sing it that way and Nathanson later caved to this oral tradition by introducing a 3/4 bar here.\n
+        3. There is a freigish-like motif which appears at "baavur sh'mo hagadol". In his full birkat hamazon setting, this motif appears several more times, but since it only appears once in the first paragraph, it is not surprising it has been smoothed out and lost over time.\n
+        There is a certain irony in that some communities refuse to use Debbie Friedman's havdala melody on the basis of her identity, but if these same people knew that this birkat hamazon melody was written by Nathanson, they would be bewildered. Nathanson was the cantor at the Society for the Advancement of Judaism, the flagship synagogue of Reconstructionist Judaism. This is a movement which at the time taught that "God is the sum of all natural processes that allow man to become self-fulfilled." In classic orthodox theology this would be considered atheism, whilst the text of Birkat Hamazon explicitly blesses God for the food He has bestowed on us.`
+    },
+    {
       id: '508',
       name: 'Harachaman',
       author: 'Haim Kirsch',
