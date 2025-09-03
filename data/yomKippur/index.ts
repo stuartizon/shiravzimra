2cd5f0f3c822876fb6fc6742574acaffedcdb3c1
Stuart Izon <stuart.izon@gmail.com>
2025-09-03 21:53:15 +0100

    Add V'chach Haya Omer Lewandowski

--- data/yomKippur/index.ts
+++ data/yomKippur/index.ts
@@ -1,10 +1,11 @@
 import { Group } from '../types'
 import { kolNidrei } from './kolNidrei'
 import { ykMaariv } from './maariv'
+import { ykMussaf } from './mussaf'
 
 export const yomKippur: Group = {
   id: 'yk',
   name: 'Yom Kippur',
   hebrewName: 'יום כפור',
-  sections: [kolNidrei, ykMaariv]
+  sections: [kolNidrei, ykMaariv, ykMussaf]
 }
---
+++ data/yomKippur/mussaf.ts
@@ -0,0 +1,19 @@
+import { Section } from '../types'
+
+export const ykMussaf: Section = {
+  id: 'ykMussaf',
+  name: 'Yom Kippur Mussaf',
+  shortName: 'Mussaf',
+  hebrewName: 'מוסף',
+  pieces: [
+    {
+      id: '334a',
+      name: "V'chach Haya Omer",
+      author: 'Louis Lewandowski',
+      youtubeUrl: 'https://youtu.be/1ePSeOdTyGI',
+      description: `During the Temple service on Yom Kippur, the High Priest would confess his sins three times: once for himself and his immediate family, once for all the priests, and once for the entire people of Israel. The text is subtly different in these iterations, but each time the High Priest completed his confession by quoting the verse from Leviticus 16:30 and pronouncing aloud the ineffable name of God, at which point all the onlookers bowed down, something we enact in synagogue to this day on Yom Kippur.\n
+        Many people comment how boring this part of the davening is – even in the 18th century the Me'am Loez mentioned that many people would doze off during the recital of the Avoda. But this is meant to be the moment of high drama! The chazzan, clad all in white, acting as stand-in for the High Priest reenacts the service when the holiest man entered the holiest place, on the holiest day of the year.\n
+        Traditionally, this paragraph is chanted responsively, each phrase first by cantor followed by congregation, and many of the classic synagogue composers composed such melodies for V'chach Haya Omer. Lewandowski's setting is simple but thoughtfully conceived. The text for the confession includes three different words for sin – "chet", "avon", and "pesha", which repeat three times, and Lewandowski employs musical sequences here to good effect. This composition is certainly performable without a choir – indeed Lewandowski specifies that the responses are for "choir or congregation".`
+    }
+  ]
+}
