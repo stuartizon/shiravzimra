9790209f2b2e6dd6b3b2b145ad2cbaf5ab280842
Stuart Izon <stuart.izon@gmail.com>
2025-09-17 14:00:09 +0100

    Add V'chach Haya Moneh Mombach

--- data/yomKippur/mussaf.ts
+++ data/yomKippur/mussaf.ts
@@ -14,6 +14,14 @@ export const ykMussaf: Section = {
       description: `During the Temple service on Yom Kippur, the High Priest would confess his sins three times: once for himself and his immediate family, once for all the priests, and once for the entire people of Israel. The text is subtly different in these iterations, but each time the High Priest completed his confession by quoting the verse from Leviticus 16:30 and pronouncing aloud the ineffable name of God, at which point all the onlookers bowed down, something we enact in synagogue to this day on Yom Kippur.\n
         Many people comment how boring this part of the davening is – even in the 18th century the Me'am Loez mentioned that many people would doze off during the recital of the Avoda. But this is meant to be the moment of high drama! The chazzan, clad all in white, acting as stand-in for the High Priest reenacts the service when the holiest man entered the holiest place, on the holiest day of the year.\n
         Traditionally, this paragraph is chanted responsively, each phrase first by cantor followed by congregation, and many of the classic synagogue composers composed such melodies for V'chach Haya Omer. Lewandowski's setting is simple but thoughtfully conceived. The text for the confession includes three different words for sin – "chet", "avon", and "pesha", which repeat three times, and Lewandowski employs musical sequences here to good effect. This composition is certainly performable without a choir – indeed Lewandowski specifies that the responses are for "choir or congregation".`
+    },
+    {
+      id: '336a',
+      name: "V'chach Haya Moneh",
+      author: 'Julius Mombach',
+      youtubeUrl: 'https://youtu.be/B9RUFigrRFk',
+      description: `At the culmination of atonement, the high priest would sprinkle the blood, first of the sacrificial bull, then of the goat, in front of the curtain facing the Ark. In Masechet Yoma Rabbi Elazar describes seeing this curtain, stolen by the Romans, on which were several drops of blood from the bull and goat. This sprinkling had to be performed carefully, as described in Leviticus 16: upward once and downward seven times, and the high priest counted along to ensure he followed this procedure exactly. This paragraph of V'chach Haya Moneh (this is how he would count) is included twice in the Yom Kippur Avoda, once for each of these sacrifices.\n
+        As with V'chach Haya Omer, traditionally this paragraph is chanted responsively, and this is evident in many synagogue composers' scores. This setting is one of two melodies by Mombach. Whilst it is the melody I'm most familiar with, the other Mombach setting is the one which appears in the Blue Book.`
     }
   ]
 }
