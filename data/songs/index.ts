43dab49768433c263a064cbed0548d4442df327d
Stuart Izon <stuart.izon@gmail.com>
2025-12-17 13:07:55 +0000

    Add Irvine Adonai Ro'i

--- data/songs/index.ts
+++ data/songs/index.ts
@@ -2,6 +2,7 @@ import { Group } from '../types'
 import { birkatHamazon } from './birkatHamazon'
 import { havdala } from './havdala'
 import { pesachSeder } from './pesachSeder'
+import { seudaShlishit } from './seudaShlishit'
 import { zmirotDay } from './zmirotDay'
 import { zmirotNight } from './zmirotNight'
 
@@ -9,5 +10,5 @@ export const songs: Group = {
   id: 'songs',
     name: 'Songs',
     hebrewName: 'שירים',
-    sections: [birkatHamazon, zmirotNight, zmirotDay, havdala, pesachSeder]
+    sections: [birkatHamazon, zmirotNight, zmirotDay, seudaShlishit, havdala, pesachSeder]
 }
---
+++ data/songs/seudaShlishit.ts
@@ -0,0 +1,16 @@
+import { Section } from '../types'
+
+export const seudaShlishit: Section = {
+  id: 'seudaShlishit',
+  name: "Seuda Sh'lishit",
+  shortName: "Seuda Sh'lishit",
+  hebrewName: '',
+  pieces: [
+    {
+      id: '535j',
+      name: "Adonai Ro'i",
+      author: 'Jessie Seymour Irvine',
+      youtubeUrl: 'https://youtu.be/QtckDtWdAO0',
+    }
+  ]
+}
